cb93eee281330cd331bb8699b900686d86971302
Fernando Sales <fsales316@gmail.com>
2020-07-29 11:43:59 -0300

    Laravel e Angular

---
+++ backend/app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Tymon\JWTAuth\Facades\JWTAuth;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $users = User::all();
+        return response()->json($users);
+    }
+
+    public function profile(){
+        return response()->json(auth()->user());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:6',
+        ]);
+
+        if($validator->fails()){
+            return response()->json($validator->errors()->toJson(), 400);
+        }
+
+        $user = User::create([
+            'name' => $request->get('name'),
+            'email' => $request->get('email'),
+            'password' => Hash::make($request->get('password')),
+        ]);
+
+        $token = JWTAuth::fromUser($user);
+
+        return response()->json(compact('user','token'),201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ frontend/src/app/components/signup/signup.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from './../../shared/auth.service';
+import { FormBuilder, FormGroup } from "@angular/forms";
+import { AuthStateService } from 'src/app/shared/auth-state.service';
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.scss']
+})
+
+export class SignupComponent implements OnInit {
+  isSignedIn: boolean;
+  registerForm: FormGroup;
+  errors = null;
+
+  constructor(
+    private auth: AuthStateService,
+    public router: Router,
+    public fb: FormBuilder,
+    public authService: AuthService
+  ) {
+    this.registerForm = this.fb.group({
+      name: [''],
+      email: [''],
+      password: ['']
+    })
+  }
+
+  ngOnInit() { }
+
+  onSubmit() {
+    this.authService.register(this.registerForm.value).subscribe(
+      result => {
+        console.log(result)
+      },
+      error => {
+        this.errors = error.error;
+      },
+      () => {
+        this.registerForm.reset()
+        this.router.navigate(['login']);
+      }
+    )
+  }
+
+}
